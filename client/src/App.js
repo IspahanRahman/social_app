15ae2d5212856fb32587c505784bfa5aeb9b783e
IspahanRahman <ispahanrahman133@gmail.com>
2023-04-18 14:36:36 +0600

    cliend and server initial setting and done some stuff

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
+import HomePage from "scenes/homePage";
+import ProfilePage from "scenes/profilePage";
+import LoginPage from "scenes/loginPage";
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage/>}/>
+          <Route path="/home" element={<HomePage/>}/>
+          <Route path="/profile/:userId" element={<ProfilePage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
